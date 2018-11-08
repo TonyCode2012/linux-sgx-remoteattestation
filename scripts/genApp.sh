ee3b5c77e40553e520a0a6add22a6e245e9f8a74
TonyCode2012 <TonyCode2012@126.com>
2018-11-08 23:43:24 +0800

    fix old code

---
+++ scripts/genApp.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+basedir=`dirname $0`
+basedir=`cd $basedir;pwd`
+
+AppDir=$basedir/../Application
+SerDir=$basedir/../ServiceProvider
+
+echo "[INFO] making service provider..."
+cd $SerDir
+make clean
+make
+
+echo "[INFO] making enclave application..."
+cd $AppDir
+make clean
+make SGX_MODE=HW SGX_PRERELEASE=1
